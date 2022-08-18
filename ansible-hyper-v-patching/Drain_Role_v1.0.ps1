5ca96beeb6fd42d1cdef6cd66b9bd4225dc4eb71
Hasshim Ali <hasshim.ali@ibm.com>
2022-08-18 11:10:13 +0800

    Added ansible files

---
+++ ansible-hyper-v-patching/Drain_Role_v1.0.ps1
@@ -0,0 +1,37 @@
+﻿$blade = $(hostname)
+
+# Fucntion
+function GetTotalVms {
+    Get-ClusterResource | where {($_.ResourceType) -like "Virtual Machine"} | where {($_.OwnerNode) -like $blade} |Select OwnerGroup, OwnerNode, State | Sort-Object OwnerNode -Descending
+}
+
+#Delet existing log files
+$logPath = "C:\ansible\*"
+if (Test-Path $logPath -Include *_log*.txt) 
+{
+  Remove-Item C:\ansible\*_log.txt*
+}
+
+#Get existing VMs before drain
+$beforeDrain = GetTotalVms
+$beforeDrain | Out-File c:\ansible\BeforeDrain_log.txt
+
+
+#Drain node and put to pause state
+Suspend-ClusterNode -Name "$blade" -Drain
+Do {
+    Write-Host (get-clusternode –Name "$blade").DrainStatus -ForegroundColor Magenta    
+    Sleep 10
+} 
+until ((get-clusternode –Name "$blade").DrainStatus -ne "InProgress")
+If ((get-clusternode –Name "$blade").DrainStatus -eq "Completed")
+{
+    #Get no. of VMs on blade and log
+    $totalVMS = GetTotalVms
+    If ($totalVMS.Count -gt 0) {
+        GetTotalVms | Out-File C:\ansible\DrainError_log.txt
+    } else {
+        $totalVMS.Count | Out-File C:\ansible\DrainSuccess_log.txt
+    }
+}
+
---
+++ ansible-hyper-v-patching/Failback_Role_v1.0.ps1
@@ -0,0 +1,26 @@
+﻿$blade = $(hostname)
+$beforeDrain = Get-Content c:\ansible\BeforeDrain_log.txt
+
+# Fucntion
+function GetTotalVms {
+    Get-ClusterResource | where {($_.ResourceType) -like "Virtual Machine"} | where {($_.OwnerNode) -like $blade} |Select OwnerGroup, OwnerNode, State | Sort-Object OwnerNode -Descending
+}
+
+Resume-clusternode -Name "$blade" -Failback Immediate
+$vmStatus = $(Get-ClusterResource).state
+while ($vmStatus -contains "OfflinePending") {
+    Write-Host "Live migration in progress" -ForegroundColor Magenta
+    Sleep 10
+    $vmStatus = $(Get-ClusterResource).state
+}
+
+Write-Host "Fail back to $blade is DONE!" -ForegroundColor Green
+
+GetTotalVms | Out-File c:\ansible\AfterDrain_log.txt
+$afterDrain = Get-Content c:\ansible\AfterDrain_log.txt
+
+if ($beforeDrain.Count -eq $afterDrain.Count) {
+    Out-File c:\ansible\Failback_Success_Log.txt
+} else {
+    Out-File c:\ansible\Failback_Error_Log.txt
+}
